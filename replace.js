94e3d5ff7ceab8e6e7e01f713d1dc9cf991b0a1e
Ken M. Penner <kpenner@stfx.ca>
2022-12-16 15:27:07 -0400

    Update replace.js
    Added Example to change a non-dropping particle (at the beginning of the family name) to a dropping particle (at the end of given name):

--- replace.js
+++ replace.js
@@ -4,6 +4,11 @@
 // combined with code from https://forums.zotero.org/discussion/7707/find-and-replace-on-multiple-items/p2
 // by tanaree https://forums.zotero.org/profile/1648161/tanaree April 17, 2014
 
+// Example to replace two spaces with one in titles:
+//var fieldName = "title";
+//var regExFilt = /  /gm;
+//var replaceText = " ";
+
 // For creators, sample JSON is [{"fieldMode":0,"firstName":"Israel","lastName":"Knohl","creatorTypeID":1}]
 // so the regExFil should include the colon and double quotes like /"firstName":"Ken(neth)?"/gm
 // and the replaceText should be in single quotes and double quotes like '"firstName":"Ken M."'
@@ -12,6 +17,16 @@
 //var fieldName = "creator";
 //var regExFilt = /(.+[a-z]),(",)/g;
 //var replaceText = "$1$2";
+
+// Example to lowercase dropping particles (at the end of given name) in a creator's name:
+//const fieldName = "creator";
+//const regExFilt = /("firstName":".+)(De|Van|Der|Le|Von)( |")/gm;
+//const replaceText = (match, p1, p2, p3) => p1 + p2.toLowerCase() + p3;
+
+// Example to change a non-dropping particle (at the beginning of the family name) to a dropping particle (at the end of given name):
+//const fieldName = "creator";
+//const regExFilt = /("firstName":"[A-Za-z ]+)(","lastName":")([Dd]e|[Vv]an|[Dd]er|[Ll]e|[Vv]on) /gm;
+//const replaceText = "$1 $3$2"
 
 
 var fieldName = "title";
